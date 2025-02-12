2ff6dcac129c6bcb751c96808bf133a43486d9c5
KechenLiu <kechen@KechenLius-Laptop.local>
2025-02-12 21:05:56 +0000

    add more fun facts

--- components/fun/Fun.tsx
+++ components/fun/Fun.tsx
@@ -12,7 +12,7 @@ function Fun({ overwriteFun }: FunProps) {
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-3 items-start">
       {FunList.map((item) => {
-        return <FunCard key={item.id} fun={item} />;
+        return <FunCard key={fun.id} fun={item} />;
       })}
     </div>
   );
--- components/fun/FunCard.tsx
+++ components/fun/FunCard.tsx
@@ -6,11 +6,13 @@ function FunCard({ fun }) {
       className="max-w-sm mx-auto flex flex-col experiences-center md:experiences-start md:justify-center"
       key={fun.id}
     >
+      <div className="w-full h-64 overflow-hidden flex justify-center items-center">
         <img
-          className="w-full rounded-md"
+          className="w-auto h-full object-cover rounded-md"
           src={fun.img}
+          alt="Fun Image"
         />
-
+      </div>
       <div className="w-full mt-5">
         <p className="text-fun-gray text-left text-sm">{fun.desc}</p>
       </div>
