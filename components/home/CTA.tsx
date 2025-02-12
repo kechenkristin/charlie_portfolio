6b3defd2380a40e409655ab3b6481d3050ec2c57
KechenLiu <kechen@KechenLius-Laptop.local>
2025-02-12 17:39:11 +0000

    add cv feature

--- components/home/CTA.tsx
+++ components/home/CTA.tsx
@@ -4,17 +4,15 @@ function CTA() {
   return (
     <div className="pt-36 relative w-full">
       <img className="w-30 m-auto mb-2" src="/static/doodles/lineBreak.svg" />
+
       <div className="pt-14 pb-40">
-        <h2 className="text-4xl md:text-5xl font-bold mb-10">
-          Interested in Working Together?
-        </h2>
-        <a
-          href="https://www.linkedin.com/in/charlie-laing-17695a314/"
-          className="cursor-pointer font-bold whitespace-nowrap 
-        mt-6 px-8 py-3 text-white border-2 rounded-full border-white bg-bg hover:bg-fun-pink hover:border-fun-pink transition-colors"
-        >
-          Get in Touch
-        </a>
+      <h2 className="text-4xl md:text-5xl font-bold mb-10">
+        View My CV
+      </h2>
+      <iframe
+        src="/docs/CV.pdf"
+        className="w-full h-[500px] border-2 border-white rounded-lg"
+      ></iframe>
       </div>
 
       <img
@@ -22,6 +20,20 @@ function CTA() {
         style={{ zIndex: "-10" }}
         src="/static/doodles/hero/fancyLines.svg"
       />
+
+
+    <div className="pt-14 pb-40">
+      <h2 className="text-4xl md:text-5xl font-bold mb-10">
+        Interested in Working Together?
+      </h2>
+      <a
+        href="https://www.linkedin.com/in/charlie-laing-17695a314/"
+        className="cursor-pointer font-bold whitespace-nowrap 
+      mt-6 px-8 py-3 text-white border-2 rounded-full border-white bg-bg hover:bg-fun-pink hover:border-fun-pink transition-colors"
+      >
+        Get in Touch
+      </a>
+      </div>
     </div>
   );
 }
